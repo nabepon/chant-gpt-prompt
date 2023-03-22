91effd69002fed3f999a643be147100102f2421f
nabepon <siokosyo@gmail.com>
2023-03-22 21:16:10 +0900

    fix keybord event

--- src/GptPromptView/enterSubmitHandler.ts
+++ src/GptPromptView/enterSubmitHandler.ts
@@ -1,6 +1,7 @@
 import React from "react";
 
 export const enterSubmitHandler = (callback: () => Promise<void>) => async (event: React.KeyboardEvent) => {
+  event.stopPropagation();
   if (event.key === 'Enter' && (event.ctrlKey || event.metaKey)) {
     event.preventDefault();
     await callback();
